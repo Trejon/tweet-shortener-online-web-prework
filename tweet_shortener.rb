37057e25d93a19ffcd38fa80bc070fab0d119ba6
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-06-09 23:54:26 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,11 +1,11 @@
 # Write your code here.
 def dictionary
   hash = {
-    "Hello" => 'hi',
+    "Hello" => "hi",
     "to" => '2', 
-    "two" => "2",
-    "too" => "2",
-    "for" => "4",
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
     "four" =>'4',
     'be' => 'b',
     'you' =>'u',
